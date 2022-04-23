4f68e04767fabba4d1fb10f190493f44b8f9abaa
jseia <joacoseia18@gmail.com>
2022-04-23 06:57:09 +0200

    working versions of feature extractors

--- models/bria2014/feature_extraction.py
+++ models/bria2014/feature_extraction.py
@@ -1,8 +1,16 @@
-from numba import jit
+from numba import njit
 import numpy as np
+import typing as tp
+import feature_modules as fm
 
 
-@jit(nopython=True)
+WINDOW_SIZE = 14
+Size = tp.NamedTuple('Size', [('height', int), ('width', int)])
+RotSize = tp.NamedTuple('RotSize', [('dx', int), ('dy', int), ('z', int)])
+Location = tp.NamedTuple('Location', [('top', int), ('left', int)])
+
+
+@njit
 def to_integral(img_arr):
     shape = img_arr.shape
     row_sum = np.zeros(shape)
@@ -11,10 +19,10 @@ def to_integral(img_arr):
         for y in range(shape[0]):
             row_sum[y, x] = row_sum[y-1, x] + img_arr[y, x]
             int_img[y+1, x+1] = int_img[y+1, x-1+1] + row_sum[y, x]
-    return int_img.astype(np.int8)
+    return int_img  #.astype(np.int8)
 
 
-@jit(nopython=True)
+@njit
 def to_diag_integral(img_arr):
     shape = img_arr.shape
     diag_int_img = np.zeros((shape[0] + 3, shape[1] + 3))
@@ -28,209 +36,86 @@ def to_diag_integral(img_arr):
                 img_arr_[y, x+1]
             diag_int_img[y+2, 1] = diag_int_img[y+1, 2]
             diag_int_img[y+2, -1] = diag_int_img[y+1, -2]
-    # diag_int_img [:, 1] = 0
-    # diag_int_img [:, -1] = 0
-    return diag_int_img[:-1, 1:].astype(np.int8)
+    return diag_int_img[1:-1, 1:-1]  #.astype(np.int8)
 
 
-
-class Box:
-    # Class `Box` that allows to determine the integral of an image region by
-    # means of the integral image.
-    def __init__(self, x: int, y: int, width: int, height: int):
-        self.coords_x = [x, x + width, x,          x + width]
-        self.coords_y = [y, y,         y + height, y + height]
-        self.coeffs = [1, -1,        -1,         1]
-
-    def __call__(self, integral_image: np.ndarray) -> float:
-        return np.dot(integral_image[self.coords_y, self.coords_x], self.coeffs)
+def possible_position(
+    size: int, window_size: int = WINDOW_SIZE
+) -> tp.Iterable[int]:
+    return range(0, window_size - size + 1)
 
 
-class Feature:
-    def __init__(self, x: int, y: int, width: int, height: int):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-
-    def __call__(self, integral_image: np.ndarray) -> float:
-        try:
-            return np.dot(integral_image[self.coords_y, self.coords_x], self.coeffs)
-        except IndexError as e:
-            raise IndexError(str(e) + ' in ' + str(self))
-
-    def __repr__(self):
-        return f'{self.__class__.__name__}(x={self.x}, ' \
-            f'y={self.y}, width={self.width}, height={self.height})'
+def possible_locations(
+    base_shape: Size, window_size: int = WINDOW_SIZE
+) -> tp.Iterable[Location]:
+    return (Location(left=x, top=y)
+            for x in possible_position(base_shape.width, window_size)
+            for y in possible_position(base_shape.height, window_size))
 
 
-class Feature2h(Feature):
-    def __init__(self, x: int, y: int, width: int, height: int):
-        super().__init__(x, y, width, height)
-        hw = width // 2
-        self.coords_x = [x,      x + hw,     x,          x + hw,
-                         x + hw, x + width,  x + hw,     x + width]
-        self.coords_y = [y,      y,          y + height, y + height,
-                         y,      y,          y + height, y + height]
-        self.coeffs = [1,     -1,         -1,          1,
-                       -1,     1,          1,         -1]
+def possible_shapes(base_shape: Size, window_size: int = WINDOW_SIZE) -> tp.Iterable[Size]:
+    base_height = base_shape.height
+    base_width = base_shape.width
+    return (Size(height=height, width=width)
+            for width in range(base_width, window_size + 1, base_width)
+            for height in range(base_height, window_size + 1, base_height))
 
 
-class Feature2v(Feature):
-    def __init__(self, x: int, y: int, width: int, height: int):
-        super().__init__(x, y, width, height)
-        hh = height // 2
-        self.coords_x = [x,      x + width,  x,          x + width,
-                         x,      x + width,  x,          x + width]
-        self.coords_y = [y,      y,          y + hh,     y + hh,
-                         y + hh, y + hh,     y + height, y + height]
-        self.coeffs = [1,     -1,         -1,          1,
-                       -1,     1,          1,         -1]
+def possible_locations_rot(
+    base_shape: RotSize, window_size: int = WINDOW_SIZE
+) -> tp.Iterable[Location]:
+    return (Location(left=x, top=y)
+            for x in possible_position(base_shape.z, window_size)
+            for y in possible_position(base_shape.z, window_size))
 
 
-class Feature3h(Feature):
-    def __init__(self, x: int, y: int, width: int, height: int):
-        super().__init__(x, y, width, height)
-        tw = width // 3
-        self.coords_x = [x,        x + tw,    x,          x + tw,
-                         x + tw,   x + 2*tw,  x + tw,     x + 2*tw,
-                         x + 2*tw, x + width, x + 2*tw,   x + width]
-        self.coords_y = [y,        y,         y + height, y + height,
-                         y,        y,         y + height, y + height,
-                         y,        y,         y + height, y + height]
-        self.coeffs = [1,      -1,        -1,          1,
-                       -1,       1,         1,         -1,
-                       1,      -1,        -1,          1]
+def possible_shapes_rot(
+    base_shape: Size, window_size: int = WINDOW_SIZE
+) -> tp.Iterable[RotSize]:
+    base_z = base_shape.height
+    return (RotSize(dx=dx, dy=dy, z=z)
+            for z in range(base_z, window_size + 1, base_z)
+            for dx in range(1, z)
+            for dy in range(1, z))
 
 
-class Feature3v(Feature):
-    def __init__(self, x: int, y: int, width: int, height: int):
-        super().__init__(x, y, width, height)
-        th = height // 3
-        self.coords_x = [x,        x + width,  x,          x + width,
-                         x,        x + width,  x,          x + width,
-                         x,        x + width,  x,          x + width]
-        self.coords_y = [y,        y,          y + th,     y + th,
-                         y + th,   y + th,     y + 2*th,   y + 2*th,
-                         y + 2*th, y + 2*th,   y + height, y + height]
-        self.coeffs = [1,       -1,        -1,          1,
-                       -1,        1,         1,         -1,
-                       1,       -1,        -1,          1]
+def feature_instantiator(window_size: int = WINDOW_SIZE, mode: str = 'all'):
+    features = []
+    if mode != 'rot':
+        features_types = [
+            (fm.Feature2h, 1, 2), (fm.Feature2v, 2, 1), (fm.Feature3h, 1, 3),
+            (fm.Feature3v, 3, 1), (fm.Feature4h, 1, 4), (fm.Feature4v, 4, 1),
+            (fm.Feature2h2v, 2, 2), (fm.Feature3h3v, 3, 3)
+        ]
+        features = []
+        for feat in features_types:
+            features.extend(
+                list(feat[0](location.left, location.top, shape.width, shape.height)
+                     for shape in possible_shapes(Size(height=feat[1], width=feat[2]), window_size)
+                     for location in possible_locations(shape, window_size))
+            )
+    features_rot = []
+    if mode != 'hor':
+        def base_iterator(feat: fm.FeatureRot):
+            return (feat(location.left, location.top, shape.dx, shape.dy, shape.z)
+                    for shape in possible_shapes_rot(Size(height=1, width=1), window_size)
+                    for location in possible_locations_rot(shape, window_size))
+
+        rot_features_types = [
+            fm.Feature2hRot, fm.Feature2vRot, fm.Feature3hRot, fm.Feature3vRot,
+            fm.Feature4hRot, fm.Feature4vRot, fm.Feature2h2vRot, fm.Feature3h3vRot
+        ]
+        features_rot = []
+        for feat in rot_features_types:
+            feature_rot = (map(lambda feat: feat if feat.plausible else None, base_iterator(feat)))
+            features_rot.extend([feat for feat in feature_rot if feat is not None])
+
+    return features + features_rot
 
 
-class Feature4h(Feature):
-    def __init__(self, x: int, y: int, width: int, height: int):
-        super().__init__(x, y, width, height)
-        tw = width // 4
-        self.coords_x = [x,        x + tw,    x,          x + tw,
-                         x + tw,   x + 3*tw,  x + tw,     x + 3*tw,
-                         x + 3*tw, x + width, x + 3*tw,   x + width]
-        self.coords_y = [y,        y,         y + height, y + height,
-                         y,        y,         y + height, y + height,
-                         y,        y,         y + height, y + height]
-        self.coeffs = [1,      -1,        -1,          1,
-                       -1,       1,         1,         -1,
-                       1,      -1,        -1,          1]
+def main():
+    print(len(feature_instantiator(14)))
 
 
-class Feature4v(Feature):
-    def __init__(self, x: int, y: int, width: int, height: int):
-        super().__init__(x, y, width, height)
-        th = height // 4
-        self.coords_x = [x,        x + width,  x,          x + width,
-                         x,        x + width,  x,          x + width,
-                         x,        x + width,  x,          x + width]
-        self.coords_y = [y,        y,          y + th,     y + th,
-                         y + th,   y + th,     y + 3*th,   y + 3*th,
-                         y + 3*th, y + 3*th,   y + height, y + height]
-        self.coeffs = [1,       -1,        -1,          1,
-                       -1,        1,         1,         -1,
-                       1,       -1,        -1,          1]
-
-
-class FeatureChecker(Feature):
-    def __init__(self, x: int, y: int, width: int, height: int):
-        super().__init__(x, y, width, height)
-        hw = width // 2
-        hh = height // 2
-        self.coords_x = [x,      x + hw,     x,          x + hw,
-                         x + hw, x + width,  x + hw,     x + width,
-                         x,      x + hw,     x,          x + hw,
-                         x + hw, x + width,  x + hw,     x + width]
-        self.coords_y = [y,      y,          y + hh,     y + hh,
-                         y,      y,          y + hh,     y + hh,
-                         y + hh, y + hh,     y + height, y + height,
-                         y + hh, y + hh,     y + height, y + height]
-        self.coeffs = [1,     -1,         -1,          1,
-                       -1,     1,          1,         -1,
-                       -1,     1,          1,         -1,
-                       1,    -1,         -1,          1]
-
-
-class FeatureCenter(Feature):
-    def __init__(self, x: int, y: int, width: int, height: int):
-        super().__init__(x, y, width, height)
-        tw = width // 3
-        th = height // 3
-
-        self.coords_x = [x,        x + width,  x,          x + width,
-                         x,        x + tw,     x,          x + tw,
-                         x + tw,   x + 2 * tw, x + tw,     x + 2 * tw,
-                         x + 2*tw, x + width,  x + 2*tw,   x + width,
-                         x,        x + width,  x,          x + width]
-        self.coords_y = [y,        y,          y + th,     y + th,
-                         y + th,   y + th,     y + 2*th,   y + 2*th,
-                         y + th,   y + th,     y + 2*th,   y + 2*th,
-                         y + th,   y + th,     y + 2*th,   y + 2*th,
-                         y + 2*th, y + 2*th,   y + height, y + height]
-        self.coeffs = [1,     -1,        -1,        1,
-                       1,     -1,        -1,        1,
-                       -1,      1,         1,       -1,
-                       1,     -1,        -1,        1,
-                       1,     -1,        -1,        1]
-
-
-class Diamond:
-    # Class `Diamond` that allows to determine the integral of an image region by
-    # means of the integral image.
-    def __init__(self, x: int, y: int, z: int, r: float):
-        w = int(z * r)  # right half
-        x = x + 1
-        y = y + 2
-        self.coords_x = np.array([x + w,      x,              x + z,      x + (z - w)])
-        self.coords_y = np.array([y + z - 1,  y + z - w - 1,   y + w - 1,  y - 1])
-        self.coords_x[[1, 3]] = self.coords_x[[1, 3]] - 1
-        self.coords_y[[1, 3]] = self.coords_y[[1, 3]] - 1
-        self.coeffs = [1, -1, -1, 1]
-
-    def __call__(self, integral_image: np.ndarray) -> float:
-        # print(integral_image[self.coords_y, self.coords_x])
-        return np.dot(integral_image[self.coords_y, self.coords_x], self.coeffs)
-
-
-class FeatureRot:
-    def __init__(self, x: int, y: int, z: int, r: float):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.r = r
-        # w = int(z * self.r)
-        # Default feature is Diamond
-        # x = x + 1
-        # y = y + 2
-        # self.coords_x = np.array([x + w,      x ,              x + z,      x + (z - w)])
-        # self.coords_y = np.array([y + z - 1,  y + z - w - 1,   y + w - 1,  y - 1])
-        # self.coords_x[[1, 3]] = self.coords_x[[1, 3]] - 1
-        # self.coords_y[[1, 3]] = self.coords_y[[1, 3]] - 1
-        # self.coeffs = [1, -1, -1, 1]
-
-    def __call__(self, integral_image: np.ndarray) -> float:
-        try:
-            print(integral_image[self.coords_y, self.coords_x])
-            return np.dot(integral_image[self.coords_y, self.coords_x], self.coeffs)
-        except IndexError as e:
-            raise IndexError(str(e) + ' in ' + str(self))
-
-    def __repr__(self):
-        return f'{self.__class__.__name__}(x={self.x}, ' \
-            f'y={self.y}, side_size={self.z}, ratio={self.r})'
+if __name__ == '__main__':
+    main()
--- models/bria2014/feature_extraction_tests.py
+++ models/bria2014/feature_extraction_tests.py
@@ -1,84 +1,189 @@
-import feature_extraction as fe
+import feature_modules as fm
 import numpy as np
 
+
 test_image = np.array([
-    [5, 2, 3, 4, 1],
-    [1, 5, 4, 2, 3],
-    [2, 2, 1, 3, 4],
-    [3, 5, 6, 4, 5],
-    [4, 1, 3, 2, 6]])
-test_integral = fe.to_integral(test_image)
+    [5, 2, 3, 4, 1, 4, 1, 4, 1, 1, 4, 1],
+    [1, 5, 4, 2, 3, 2, 3, 2, 3, 3, 2, 3],
+    [2, 2, 1, 3, 4, 3, 4, 3, 4, 4, 3, 4],
+    [3, 5, 6, 4, 5, 4, 5, 4, 5, 5, 4, 5],
+    [4, 1, 3, 2, 6, 2, 6, 2, 6, 6, 2, 6],
+    [3, 5, 6, 4, 5, 4, 5, 4, 5, 5, 4, 5],
+    [4, 1, 3, 2, 6, 2, 6, 2, 6, 6, 2, 6],
+    [3, 5, 6, 4, 5, 4, 5, 4, 5, 5, 4, 5],
+    [4, 1, 3, 2, 6, 2, 6, 2, 6, 6, 2, 6],
+    [4, 1, 3, 2, 6, 2, 6, 2, 6, 6, 2, 6],
+    [3, 5, 6, 4, 5, 4, 5, 4, 5, 5, 4, 5],
+    [4, 1, 3, 2, 6, 2, 6, 2, 6, 6, 2, 6]]
+)
+test_integral = fm.to_integral(test_image)
+test_integral_dig = fm.to_diag_integral(test_image)
 
 
 def test_Box(x, y, w, h):
     expected = np.sum(test_image[y:h+y, x:w+x])
-    test_box = fe.Box(x, y, w, h)
+    test_box = fm.Box(x, y, w, h)
     actual = test_box(test_integral)
     assert expected == actual, f'test_Box failed: {expected} == {actual}'
+    return True
 
 
 def test_Feature2h():
-    expected = fe.Box(0, 1, 2, 3)(test_integral) - fe.Box(2, 1, 2, 3)(test_integral)
-    actual = fe.Feature2h(0, 1, 4, 3)(test_integral)
+    expected = fm.Box(0, 1, 2, 3)(test_integral) - fm.Box(2, 1, 2, 3)(test_integral)
+    actual = fm.Feature2h(0, 1, 4, 3)(test_integral)
     assert expected == actual, f'test Features2h failed: {expected} == {actual}'
+    return True
 
 
 def test_Feature2v():
-    expected = fe.Box(0, 1, 4, 2)(test_integral) - fe.Box(0, 3, 4, 2)(test_integral)
-    actual = fe.Feature2v(0, 1, 4, 4)(test_integral)
+    expected = fm.Box(0, 1, 4, 2)(test_integral) - fm.Box(0, 3, 4, 2)(test_integral)
+    actual = fm.Feature2v(0, 1, 4, 4)(test_integral)
     assert expected == actual, f'test Features2h failed: {expected} == {actual}'
+    return True
 
 
 def test_Feature3h():
-    expected = fe.Box(0, 0, 1, 2)(test_integral) \
-        - fe.Box(1, 0, 1, 2)(test_integral) + fe.Box(2, 0, 1, 2)(test_integral)
-    actual = fe.Feature3h(0, 0, 3, 2)(test_integral)
+    expected = -fm.Box(0, 0, 1, 2)(test_integral) \
+        + fm.Box(1, 0, 1, 2)(test_integral) - fm.Box(2, 0, 1, 2)(test_integral)
+    actual = fm.Feature3h(0, 0, 3, 2)(test_integral)
     assert expected == actual, f'test Features3h failed: {expected} == {actual}'
+    return True
 
 
 def test_Feature3v():
-    expected = fe.Box(0, 0, 2, 1)(test_integral) \
-        - fe.Box(0, 1, 2, 1)(test_integral) + fe.Box(0, 2, 2, 1)(test_integral)
-    actual = fe.Feature3v(0, 0, 2, 3)(test_integral)
-    assert expected == actual, f'test Features3h failed: {expected} == {actual}'
+    expected = - fm.Box(0, 0, 2, 1)(test_integral) \
+        + fm.Box(0, 1, 2, 1)(test_integral) - fm.Box(0, 2, 2, 1)(test_integral)
+    actual = fm.Feature3v(0, 0, 2, 3)(test_integral)
+    assert expected == actual, f'test Features3v failed: {expected} == {actual}'
+    return True
 
 
 def test_Feature4h():
-    expected = fe.Box(0, 0, 1, 2)(test_integral) \
-        - fe.Box(1, 0, 2, 2)(test_integral) + fe.Box(3, 0, 1, 2)(test_integral)
-    actual = fe.Feature4h(0, 0, 4, 2)(test_integral)
-    assert expected == actual, f'test Features3h failed: {expected} == {actual}'
+    expected = - fm.Box(0, 0, 1, 2)(test_integral) \
+        + fm.Box(1, 0, 2, 2)(test_integral) - fm.Box(3, 0, 1, 2)(test_integral)
+    actual = fm.Feature4h(0, 0, 4, 2)(test_integral)
+    assert expected == actual, f'test Features4h failed: {expected} == {actual}'
+    return True
 
 
 def test_Feature4v():
-    expected = fe.Box(0, 0, 2, 1)(test_integral) \
-        - fe.Box(0, 1, 2, 2)(test_integral) + fe.Box(0, 3, 2, 1)(test_integral)
-    actual = fe.Feature4v(0, 0, 2, 4)(test_integral)
-    assert expected == actual, f'test Features3h failed: {expected} == {actual}'
+    expected = - fm.Box(0, 0, 2, 1)(test_integral) \
+        + fm.Box(0, 1, 2, 2)(test_integral) - fm.Box(0, 3, 2, 1)(test_integral)
+    actual = fm.Feature4v(0, 0, 2, 4)(test_integral)
+    assert expected == actual, f'test Features4v failed: {expected} == {actual}'
+    return True
 
 
-def test_FeatureChecker():
-    expected = fe.Box(0, 0, 1, 1)(test_integral) - fe.Box(0, 1, 1, 1)(test_integral) \
-        - fe.Box(1, 0, 1, 1)(test_integral) + fe.Box(1, 1, 1, 1)(test_integral)
-    actual = fe.FeatureChecker(0, 0, 2, 2)(test_integral)
-    assert expected == actual, f'test Features3h failed: {expected} == {actual}'
+def test_Feature2h2v():
+    expected = + fm.Box(0, 0, 1, 1)(test_integral) - fm.Box(0, 1, 1, 1)(test_integral) \
+        - fm.Box(1, 0, 1, 1)(test_integral) + fm.Box(1, 1, 1, 1)(test_integral)
+    actual = fm.Feature2h2v(0, 0, 2, 2)(test_integral)
+    assert expected == actual, f'test Features2h2v failed: {expected} == {actual}'
+    return True
 
 
-def test_FeatureCenter():
-    expected = fe.Box(0, 0, 3, 1)(test_integral) + fe.Box(0, 1, 1, 1)(test_integral) \
-        - fe.Box(1, 1, 1, 1)(test_integral) + fe.Box(2, 1, 1, 1)(test_integral) \
-        + fe.Box(0, 2, 3, 1)(test_integral)
-    actual = fe.FeatureCenter(0, 0, 3, 3)(test_integral)
-    assert expected == actual, f'test fe.Features3h failed: {expected} == {actual}'
+def test_Feature3h3v():
+    expected = - fm.Box(0, 0, 3, 1)(test_integral) - fm.Box(0, 1, 1, 1)(test_integral) \
+        + fm.Box(1, 1, 1, 1)(test_integral) - fm.Box(2, 1, 1, 1)(test_integral) \
+        - fm.Box(0, 2, 3, 1)(test_integral)
+    actual = fm.Feature3h3v(0, 0, 3, 3)(test_integral)
+    assert expected == actual, f'test fm.Features3h3v failed: {expected} == {actual}'
+    return True
 
 
 def test_Diamond():
-    expected = 2+3+4+1+5+4+2+3+2+2+1+3+4+5+6+4+3
-    actual = fe.Diamond(0, 0, 5, 0.5)(test_integral)
-    assert expected == actual, f'test_Box failed: {expected} == {actual}'
+    expected = 3+4+5+2+2+3
+    actual = fm.Diamond(3, 0, 1, 3, 12)(test_integral_dig)
+    assert expected == actual, f'test_Diamond failed: {expected} == {actual}'
+    return True
 
 
-def test_FeatureRot():
-    expected = 2+3+4+1+5+4+2+3+2+2+1+3+4+5+6+4+3
-    actual = fe.FeatureRot(0, 0, 5, 0.5)(test_integral)
-    assert expected == actual, f'test Features2h failed: {expected} == {actual}'
+def test_Feature2hRot():
+    expected = 2 * fm.Diamond(3, 0, 1, 3, 12)(test_integral_dig) - \
+        fm.Diamond(3, 0, 2, 3, 12)(test_integral_dig)
+    actual = fm.Feature2hRot(3, 0, 1, 3, 12)(test_integral_dig)
+    assert expected == actual, f'test_Feature2hRot failed: {expected} == {actual}'
+    return True
+
+
+def test_Feature2vRot():
+    expected = 2 * fm.Diamond(2, 0, 2, 1, 12)(test_integral_dig) - \
+        fm.Diamond(2, 0, 2, 2, 12)(test_integral_dig)
+    actual = fm.Feature2vRot(2, 0, 2, 1, 12)(test_integral_dig)
+    assert expected == actual, f'test_Feature2vRot failed: {expected} == {actual}'
+    return True
+
+
+def test_Feature3hRot():
+    expected = 2 * fm.Diamond(5, 1, 1, 3, 12)(test_integral_dig) - \
+        fm.Diamond(4, 0, 3, 3, 12)(test_integral_dig)
+    actual = fm.Feature3hRot(4, 0, 1, 3, 12)(test_integral_dig)
+    assert expected == actual, f'test_Feature3hRot failed: {expected} == {actual}'
+    return True
+
+
+def test_Feature3vRot():
+    expected = 2 * fm.Diamond(3, 1, 3, 1, 12)(test_integral_dig) - \
+        fm.Diamond(4, 0, 3, 3, 12)(test_integral_dig)
+    actual = fm.Feature3vRot(4, 0, 3, 1, 12)(test_integral_dig)
+    assert expected == actual, f'test_Feature3vRot failed: {expected} == {actual}'
+    return True
+
+
+def test_Feature4hRot():
+    expected = 2 * fm.Diamond(5, 1, 2, 3, 12)(test_integral_dig) - \
+        fm.Diamond(4, 0, 4, 3, 12)(test_integral_dig)
+    actual = fm.Feature4hRot(4, 0, 1, 3, 12)(test_integral_dig)
+    assert expected == actual, f'test_Feature4hRot failed: {expected} == {actual}'
+    return True
+
+
+def test_Feature4vRot():
+    expected = 2 * fm.Diamond(3, 1, 3, 2, 12)(test_integral_dig) - \
+        fm.Diamond(4, 0, 3, 4, 12)(test_integral_dig)
+    actual = fm.Feature4vRot(4, 0, 3, 1, 12)(test_integral_dig)
+    assert expected == actual, f'test_Feature4vRot failed: {expected} == {actual}'
+    return True
+
+
+def test_Feature2h2vRot():
+    expected = 2 * fm.Diamond(4, 0, 2, 2, 12)(test_integral_dig) + \
+        2 * fm.Diamond(4, 4, 2, 2, 12)(test_integral_dig) - \
+        fm.Diamond(4, 0, 4, 4, 12)(test_integral_dig)
+    actual = fm.Feature2h2vRot(4, 0, 2, 2, 12)(test_integral_dig)
+    assert expected == actual, f'test_Feature2h2vRot failed: {expected} == {actual}'
+    return True
+
+
+def test_Feature3h3vRot():
+    expected = 2 * fm.Diamond(3, 1, 1, 1, 12)(test_integral_dig) - \
+        fm.Diamond(4, 0, 3, 3, 12)(test_integral_dig)
+    actual = fm.Feature3h3vRot(4, 0, 1, 1, 12)(test_integral_dig)
+    assert expected == actual, f'test_Feature3h3vRot failed: {expected} == {actual}'
+    return True
+
+
+def main():
+    print(f'Test Box passed? {test_Box(0, 0, 3, 3)}')
+    print(f'Test Feature2h passed? {test_Feature2h()}')
+    print(f'Test Feature2v passed? {test_Feature2v()}')
+    print(f'Test Feature3h passed? {test_Feature3h()}')
+    print(f'Test Feature3v passed? {test_Feature3v()}')
+    print(f'Test Feature4h passed? {test_Feature4h()}')
+    print(f'Test Feature4v passed? {test_Feature4v()}')
+    print(f'Test Feature2h2v passed? {test_Feature2h2v()}')
+    print(f'Test Feature3h3v passed? {test_Feature3h3v()}')
+    print(f'Test Diamond passed? {test_Diamond()}')
+    print(f'Test Feature2hRot passed? {test_Feature2hRot()}')
+    print(f'Test Feature2vRot passed? {test_Feature2vRot()}')
+    print(f'Test Feature3hRot passed? {test_Feature3hRot()}')
+    print(f'Test Feature3vRot passed? {test_Feature3vRot()}')
+    print(f'Test Feature4hRot passed? {test_Feature4hRot()}')
+    print(f'Test Feature4vRot passed? {test_Feature4vRot()}')
+    print(f'Test Feature2h2vRot passed? {test_Feature2h2vRot()}')
+    print(f'Test Feature3h3vRot passed? {test_Feature3h3vRot()}')
+    print('\nALL TESTS PASSED GO GRAB A BEER TO CELEBRATE!')
+
+
+if __name__ == '__main__':
+    main()
