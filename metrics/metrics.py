add correct calculation of tp fp tn

metrics/metrics.py
@@ -1,14 +1,15 @@
 import cv2
 import numpy as np
 import math
+from scipy import spatial
+from general_utils.utils import get_center_bboxes
+from metrics.metrics_utils import evaluate_pairs_iou_appox, evaluate_pairs_iou_exact
 
 
 def circle_comparison(predicted_roi_circles, mask, return_counts=True):
     """Finds TP, FP and FN among predicted circles based on the true image mask.
-
     More precise circle comparison. Checks the intersection of each predicted circle
     with the true leision bbox and counts a TP if >=1 roi pixel is in the circle.
-
     Args:
         true_bboxes (np.ndarray): Array of shape (n_rois, 2) containing
             top_left and bottom_right bbox coordinates in tuples
@@ -17,7 +18,6 @@ def circle_comparison(predicted_roi_circles, mask, return_counts=True):
         mask (np.ndarray): Image mask containing indexes of rois
         return_counts (bool, optional): Whether to return counts of TP, FP, FN arrays
             or corresponding indexes. Defaults to True.
-
     Returns:
         If return_counts=True:
             (TP, FP, FN) (tuple[int]): number of TP, FP, FN
@@ -54,10 +54,8 @@ def circle_comparison(predicted_roi_circles, mask, return_counts=True):
 
 def quick_circle_comparison(predicted_roi_circles, mask, return_counts=True):
     """Finds TP, FP and FN among predicted circles based on the true image mask.
-
     Quick version that checks if a rectangular bbox around each circle in the image
     mask contains any roi indexes and counts a TP if >= 1 roi pixel is in that bbox.
-
     Args:
         true_bboxes (np.ndarray): Array of shape (n_rois, 2) containing
             top_left and bottom_right bbox coordinates in tuples
@@ -66,7 +64,6 @@ def quick_circle_comparison(predicted_roi_circles, mask, return_counts=True):
         mask (np.ndarray): Image mask containing indexes of rois
         return_counts (bool, optional): Whether to return counts of TP, FP, FN arrays
             or corresponding indexes. Defaults to True.
-
     Returns:
         If return_counts=True:
             (TP, FP, FN) (tuple[int]): number of TP, FP, FN
@@ -90,7 +87,8 @@ def quick_circle_comparison(predicted_roi_circles, mask, return_counts=True):
 
         # looks at pixels in the mask at the circle's bbox
         intersected_mask_idxs = set(
-            mask[cricle_tl[1]:circle_br[1], cricle_tl[0]:circle_br[0]].ravel()).difference(back_set)
+            mask[cricle_tl[1]:circle_br[1], cricle_tl[0]:circle_br[0]].ravel()
+        ).difference(back_set)
         if len(intersected_mask_idxs) > 0:
             TP = TP.union(intersected_mask_idxs)
         else:
@@ -105,26 +103,89 @@ def quick_circle_comparison(predicted_roi_circles, mask, return_counts=True):
         return TP, FP, FN
 
 
-def create_binary_mask_from_blobs(
-    original_img_shape: tuple, blobs_x_y_sigma: np.ndarray, n_jobs: int=4
+def get_tp_fp_fn(
+    lesion_bboxes: np.ndarray, radiouses: np.ndarray, detections: np.ndarray,
+    min_dist: int, min_iou: float = None, exact: bool = False,
+    lesion_mask: np.ndarray = None
 ):
-    blobs_x_y_sigma = blobs_x_y_sigma.astype('int')
-    sigmas = np.unique(blobs_x_y_sigma[:, 2])
-    # all_detections_mask = np.zeros(original_img_shape)
-    all_detections_mask = []
-    for sigma in sigmas:
-        mask = np.zeros(original_img_shape)
-        coords = blobs_x_y_sigma[np.where(blobs_x_y_sigma[:, 2] == sigma)]
-        mask[coords[:, 0], coords[:, 1]] = 1
+    """Gets the true positives, flase positives, false negatives, values from
+        ground truth actually correctly predicted and the false positives matching
+        the distance condition but not the area one.
 
-        radius = int(math.sqrt(2)*sigma)
-        kernel_size = 2*radius
-        if kernel_size % 2 != 1:
-            kernel_size += 1
-        template_circle = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, ksize=(kernel_size, kernel_size))
-        mask = cv2.dilate(mask, template_circle)
-        all_detections_mask = mask & all_detections_mask
-    return all_detections_mask
+    Args:
+        lesion_bboxes (np.ndarray): grund truth lesion bboxes array as
+            coming out from INBreastDataset item
+        radiouses (np.ndarray): grund truth lesion radiuses array as
+            coming out from INBreastDataset item
+        detections (np.ndarray): detections given by (x,y,sigma)
+        min_dist (int): minimun distance criterium between the centers of
+            candidate lesion and ground truth
+        min_iou (float): minimun intersection over union criterium
+            between the centers of candidate lesion and ground truth
+        exact (bool): If True, the true shape of the lesion is used
+            else the circular approzimation is used
+        lesion_mask (np.ndarray): if the exact method is used the lesion mask needs
+            to be used
+    Returns:
+        tp (np.ndarray): [(x,y,sigma)]
+        fp (np.ndarray): [(x,y,sigma)]
+        fn (np.ndarray): [(x,y,radius)]
+        gt_predicted (np.ndarray): ground truth lesion actually detected
+            [(x,y,radius)]
+        close_fp (np.ndarray): false positives that match distance criteria
+            but not iou (only if iou value is indicated)
+    """
+    if exact:
+        assert (lesion_mask is not None), 'Exact method requires the mask of lesions'
+
+    # Get ground truth approximate circles
+    radiouses = np.expand_dims(radiouses.astype(int), 1)
+    gt_centers = get_center_bboxes(lesion_bboxes)
+    gt_circles = np.concatenate([gt_centers, radiouses], axis=1)
+
+    # Get the distance tree
+    datapoints = np.concatenate([gt_circles, detections])
+    tree = spatial.cKDTree(datapoints)
+
+    # Get the indexes among all points of ground truth points
+    gt_idxs = np.arange(len(gt_circles))
+
+    # Get the pairs closer than the required distance
+    pairs = tree.query_pairs(min_dist)
+
+    # Get the pairs matching the intersection over union condition
+    min_iou = 1 if min_iou is None else min_iou
+    if exact:
+        tp_idx, fp_idx, detected_gts = evaluate_pairs_iou_exact(
+            pairs, gt_idxs, datapoints, min_iou, lesion_mask, lesion_bboxes
+        )
+    else:
+        tp_idx, fp_idx, detected_gts = evaluate_pairs_iou_appox(
+            pairs, gt_idxs, datapoints, min_iou
+        )
+
+    # Get TP, FP, FN and missed sets of points
+    detected_gts = list(set(detected_gts))
+    missed_idx = [idx for idx in gt_idxs if idx not in detected_gts]
+    tp = datapoints[tp_idx, :]
+    close_fp = datapoints[fp_idx, :]
+    fn = datapoints[missed_idx, :]
+    gt_predicted = datapoints[list(detected_gts), :]
+    fp_idx = np.full(len(datapoints), True)
+    fp_idx[tp_idx] = False
+    fp = datapoints[fp_idx, :]
+
+    return tp, fp, fn, gt_predicted, close_fp
+
+
+def create_binary_mask_from_blobs(shape: tuple, blobs_x_y_sigma: list):
+    img_binary_blobs = np.zeros(shape)
+    for blob in blobs_x_y_sigma:
+        img_binary_blobs = cv2.circle(
+            img_binary_blobs, (blob[1], blob[0]),
+            int(math.sqrt(2) * blob[2]), 255, -1
+        )
+    return img_binary_blobs
 
 
 def cal_det_se(no_tp, no_fn):

metrics/metrics_utils.py
@@ -1,8 +1,15 @@
-import numpy as np
+import cv2
 import math
-from numba import njit
 import logging
+import warnings
 
+import numpy as np
+
+from numba import njit
+from numba.core.errors import NumbaDeprecationWarning, NumbaPendingDeprecationWarning
+
+warnings.simplefilter('ignore', category=NumbaDeprecationWarning)
+warnings.simplefilter('ignore', category=NumbaPendingDeprecationWarning)
 logging.basicConfig(level=logging.INFO)
 
 
@@ -17,26 +24,19 @@ def clip(val):
 
 
 @njit(cache=True)
-def compute_disk_overlap(d, r1, r2):
+def compute_disk_overlap(d: float, r1: float, r2: float, iou: bool = False):
+    """Compute fraction of surface overlap between two disks of radius
+    r1 and r2, with centers separated by a distance d.
+    Args:
+        d (float): Distance between centers.
+        r1 (float): Radius of the first disk.
+        r2 (float): Radius of the second disk.
+        iou (bool): Whether to return the intersection over union.
+            Defaults to False
+    Returns:
+        (float): Fraction of area of the overlap between the two disks.
+            Or intersection over union if indicated
     """
-    Compute fraction of surface overlap between two disks of radii
-    ``r1`` and ``r2``, with centers separated by a distance ``d``.
-
-    Parameters
-    ----------
-    d : float
-        Distance between centers.
-    r1 : float
-        Radius of the first disk.
-    r2 : float
-        Radius of the second disk.
-
-    Returns
-    -------
-    fraction: float
-        Fraction of area of the overlap between the two disks.
-    """
-
     ratio1 = (d ** 2 + r1 ** 2 - r2 ** 2) / (2 * d * r1)
     ratio1 = clip(ratio1)
     acos1 = math.acos(ratio1)
@@ -51,6 +51,9 @@ def compute_disk_overlap(d, r1, r2):
     d = d + r2 + r1
     area = (r1 ** 2 * acos1 + r2 ** 2 * acos2 -
             0.5 * math.sqrt(abs(a * b * c * d)))
+    if iou:
+        return area / ((math.pi * (r1) ** 2) + (math.pi * (r2) ** 2) - area)
+
     return area / (math.pi * (min(r1, r2) ** 2))
 
 
@@ -110,14 +113,18 @@ def blob_overlap(blob1, blob2):
 
 
 @njit(cache=True)
-def compare_and_filter_pairs(pairs: np.ndarray, blobs_array: np.ndarray, overlap: float):
+def compare_and_filter_pairs(
+    pairs: np.ndarray, blobs_array: np.ndarray, overlap: float
+):
     """Check if closes detections have an overlapping greater than the threshold
     Args:
         pairs (np.ndarray): indexes of a pair of close points
-        blobs_array (np.ndarray): array with blobs as rows (x, y, sigma)
-        overlap (float): overlapping threshold
+        blobs_array (np.ndarray): array with blobs as rows
+            (y, x, sigma) = (row, col, sigma)
+        overlap (float): minimum overlap allowed
     Returns:
-        (np.ndarray): filtered array with blobs as rows (x, y, sigma)
+        (np.ndarray): filtered array with blobs as rows
+            (y, x, sigma) = (row, col, sigma)
     """
     for (i, j) in pairs:
         blob1, blob2 = blobs_array[i], blobs_array[j]
@@ -127,26 +134,151 @@ def compare_and_filter_pairs(pairs: np.ndarray, blobs_array: np.ndarray, overlap
             if blob1[-1] > blob2[-1]:
                 blob2[-1] = 0
             else:
-                logging.info(blob1[-1])
                 blob1[-1] = 0
-                # TODO: Check if this is ocurring in place
-                logging.info(blob1[-1])
-                logging.info(blobs_array[i])
     return blobs_array[np.where(blobs_array[:, -1] > 0)]
 
 
-def min_max_norm(img: np.ndarray, max_val: int = None):
+@njit(cache=True)
+def evaluate_pairs_iou_appox(
+    pairs: set, gt_idxs: list, datapoints: np.ndarray, min_iou: float
+):
     """
-    Scales images to be in range [0, 2**bits]
-
-    Args:
-        img (np.ndarray): _description_
-        max_val (int, optional): _description_. Defaults to None.
-
-    Returns:
-        _type_: _description_
+    Evaluate pairs of gt objects and detected ones over the overlap
     """
-    if max_val is None:
-        max_val = np.iinfo(img.dtype).max
-    img = (img - img.min()) / (img.max() - img.min()) * max_val
-    return img
+    fp_idx, tp_idx, detected_gts = [], [], []
+    for i, j in pairs:
+        if ((i not in gt_idxs) and (j not in gt_idxs)):
+            continue
+        gt_idx = i if ((i in gt_idxs) and (j not in gt_idxs)) else j
+        det_idx = j if (i in gt_idxs) and (j not in gt_idxs) else i
+
+        gt = datapoints[gt_idx]
+        det = datapoints[det_idx]
+
+        d = math.sqrt(np.sum((gt[:-1] - det[:-1])**2))
+        # No intersection
+        r1, r2 = gt[-1], det[-1]
+
+        # Don't check overlaping
+        if min_iou >= 1:
+            tp_idx.append(det_idx)
+            detected_gts.append(gt_idx)
+        # Check overlaping
+        else:
+            # No overlapping
+            if d > r1 + r2:
+                fp_idx.append(det_idx)
+            # Overlapping
+            else:
+                # Dot mc inside det
+                if r1 == 0:
+                    detected_gts.append(gt_idx)
+                    tp_idx.append(det_idx)
+                # Overlapping greater than threshold
+                elif compute_disk_overlap(d, r1, r2, True) > min_iou:
+                    detected_gts.append(gt_idx)
+                    tp_idx.append(det_idx)
+                    # Overlapping less than threshold
+                else:
+                    fp_idx.append(det_idx)
+    return tp_idx, fp_idx, detected_gts
+
+
+def evaluate_pairs_iou_exact(
+    pairs: set, gt_idxs: list, datapoints: np.ndarray, min_iou: float,
+    lesion_mask: np.ndarray, lesion_bboxes: np.ndarray
+):
+    """
+    Evaluate pairs of gt objects and detected ones over the overlap
+    """
+    img_shape = lesion_mask.shape
+    fp_idx, tp_idx, detected_gts = [], [], []
+    for i, j in pairs:
+        if ((i not in gt_idxs) and (j not in gt_idxs)):
+            continue
+        gt_idx = i if ((i in gt_idxs) and (j not in gt_idxs)) else j
+        det_idx = j if (i in gt_idxs) and (j not in gt_idxs) else i
+
+        gt = datapoints[gt_idx]
+        det = datapoints[det_idx]
+
+        d = math.sqrt(np.sum((gt[:-1] - det[:-1])**2))
+        # No intersection
+        r1, r2 = gt[-1], det[-1]
+
+        # Don't check overlaping
+        if min_iou >= 1:
+            tp_idx.append(det_idx)
+            detected_gts.append(gt_idx)
+        # Check overlaping
+        else:
+            # No overlapping
+            if d > r1 + r2:
+                fp_idx.append(det_idx)
+            # Overlapping
+            else:
+                # Dot mc inside det
+                if r1 == 0:
+                    detected_gts.append(gt_idx)
+                    tp_idx.append(det_idx)
+                iou = get_exact_iou(
+                    gt, gt_idx, det, img_shape, lesion_mask, lesion_bboxes
+                )
+                if iou > min_iou:
+                    detected_gts.append(gt_idx)
+                    tp_idx.append(det_idx)
+                    # Overlapping less than threshold
+                else:
+                    fp_idx.append(det_idx)
+    return tp_idx, fp_idx, detected_gts
+
+
+def get_exact_iou(
+    gt: np.ndarray, gt_idx: int, det: np.ndarray, img_shape: tuple,
+    lesion_mask: np.ndarray, lesion_bboxes: np.ndarray
+):
+    """"Given lesion and detection coordinates compute
+    the exact iou using the lesion mask"""
+    gt = gt.astype(int)
+    det = det.astype(int)
+    gt_val = lesion_mask[gt[:-1]]
+    gt_bbox = lesion_bboxes[gt_idx]
+    det_bbox = center_sigma_to_bbox(det, img_shape)
+    bbox = get_max_bbox(gt_bbox, det_bbox)
+    det_coords_adjusted = det.copy()
+    det_coords_adjusted[0] = det[0] - bbox[0][0]
+    det_coords_adjusted[1] = det[1] - bbox[0][1]
+
+    gt_patch = lesion_mask[
+        bbox[0][1]:bbox[1][1], bbox[0][0]:bbox[1][0]
+    ]
+    detection_patch = np.zeros_like(gt_patch)
+    gt_patch = np.where(gt_patch == gt_val, 1, 0)
+    detection_patch = cv2.circle(
+        detection_patch,
+        (det_coords_adjusted[0], det_coords_adjusted[1]),
+        int(det_coords_adjusted[0]), 1, -1
+    )
+    return (gt_patch & detection_patch).sum() / \
+        (gt_patch | detection_patch).sum()
+
+
+def center_sigma_to_bbox(coords: np.ndarray, img_shape: tuple):
+    """
+    Convert (x, y, s) coordinates to a bbox
+    """
+    x, y, s = coords
+    x0 = np.minimum(x - s, 0)
+    x1 = np.minimum(x + s, img_shape[0])
+    y0 = np.minimum(y - s, 0)
+    y1 = np.minimum(y + s, img_shape[1])
+    return [(x0, y0), (x1, y1)]
+
+
+def get_max_bbox(gt_bbox: np.ndarray, det_bbox: np.ndarray):
+    """Get the maimum bbox containing the two of them"""
+    x_min = np.minimum(gt_bbox[0][0], det_bbox[0][0])
+    x_max = np.maximum(gt_bbox[1][0], det_bbox[1][0])
+    y_min = np.minimum(gt_bbox[0][1], det_bbox[0][1])
+    y_max = np.maximum(gt_bbox[1][1], det_bbox[1][1])
+    return [(x_min, y_min), (x_max, y_max)]