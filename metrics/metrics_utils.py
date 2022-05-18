e499b8b6edc099cadf4a0d59b28b807970d16783
jseia <joacoseia18@gmail.com>
2022-05-18 10:20:55 +0200

    refactor metrics functions to include several TP and do NMS

--- metrics/metrics_utils.py
+++ metrics/metrics_utils.py
@@ -4,6 +4,9 @@ import logging
 import warnings
 
 import numpy as np
+import pandas as pd
+
+import general_utils.utils as utils
 
 from numba import njit
 from numba.core.errors import NumbaDeprecationWarning, NumbaPendingDeprecationWarning
@@ -310,3 +313,270 @@ def get_max_bbox(gt_bbox: np.ndarray, det_bbox: np.ndarray):
     y_min = np.minimum(gt_bbox[0][1], det_bbox[0][1])
     y_max = np.maximum(gt_bbox[1][1], det_bbox[1][1])
     return [(x_min, y_min), (x_max, y_max)]
+
+
+@njit(cache=True)
+def patches_intersections_with_labels(
+    candidates: np.ndarray, roi_mask: np.ndarray, center_region_size: int, patch_size: int,
+    binary: bool = False
+):
+    """Get the number of pixels of the intersection between the candidates and the ground
+    truth mask, looking at the center (of size center_region_size) of a patch (of size
+    patch_size) centered at each candidate.
+    If binary just return if there's intersection of not.
+    Args:
+        candidates (np.ndarray): [x, y, radius]
+        roi_mask (np.ndarray): mask of lesion labels (each one identified independently)
+        center_region_size (int): region in the center of the patch to consider for labeling.
+            If None, all the patch is cosidered.
+        patch_size (int): size of the patch to evaluate
+        binary (bool, optional): Whether to return True or False if there was an intersection
+            instead of the counts. Defaults to False.
+    Returns:
+        intersections (np.ndarray): number of pixels of intersection with the gt mask 
+            for each candidate. If Binary then this is binarized.
+    """
+    intersections = np.empty(len(candidates))
+    for coords_idx, coords in enumerate(candidates):
+        # getting patch coordinates
+        patch_x1, patch_x2, patch_y1, patch_y2 = utils.patch_coordinates_from_center(
+            (coords[0], coords[1]), roi_mask.shape, patch_size)
+
+        # getting coordinates of the patch center. Necessary if the patch is
+        # in border and shifted
+        if center_region_size is not None:
+            p_center_y = patch_y1 + (patch_y2 - patch_y1)//2
+            p_center_x = patch_x1 + (patch_x2 - patch_x1)//2
+            patch_x1, patch_x2, patch_y1, patch_y2 = utils.patch_coordinates_from_center(
+                (p_center_x, p_center_y), roi_mask.shape, center_region_size)
+
+        intersection = np.sum(roi_mask[patch_y1:patch_y2, patch_x1:patch_x2] > 0)
+        intersections[coords_idx] = intersection
+    if binary:
+        intersection = np.where(intersection > 0, 1, 0)
+    return intersections
+
+
+def get_tp_fp_fn_center_patch_criteria(
+    candidates: np.ndarray, roi_mask: np.ndarray, center_region_size: int, patch_size: int
+):
+    """
+    Given an array of candidates and the mask of labels, it computes the itersection of a
+    patch of patch_size centered on each candidate and -if the center crop of center_regio_size
+    inside that patch matches a lesion in the gt mask- its counted as a tp if not as a fp.
+    If center_region_size is None, then the intersection on the original patch is computed.
+    At the end it gets the labels that weren't matched (FN).
+    The returned dataframe for TP containes duplicated detections, for the cases in whic one
+    detection matches more than one label. To include them in the sentivity metrics and to
+    make an easier computation of froc NMS this duplicates are kept. If they are not desired
+    performing a drop_duplicates(subset=['repeted_idxs']) will do the job.
+    Args:
+        candidates (np.ndarray): [x, y, radius]
+        roi_mask (np.ndarray): mask of lesion labels (each one identified independently)
+        center_region_size (int): region in the center of the patch to consider for labeling
+        patch_size (int): size of the patch to evaluate
+    Returns:
+        tp (pd.DataFrame): Columns: ['x', 'y', 'radius', 'label', 'matching_gt','repeted_idxs']
+        fp (pd.DataFrame): Columns: ['x', 'y', 'radius', 'label', 'matching_gt','repeted_idxs']
+        fn (pd.DataFrame): Columns: ['x', 'y', 'radius', 'label', 'matching_gt','repeted_idxs']
+    """
+    tp, fp, fn = [], [], []
+    matching_gt, repeted_tp, repeted_fp = [], [], []
+    detected_labels = set()
+    tp_count, fp_count = 0, 0
+    for coords in candidates:
+        # getting patch coordinates
+        patch_x1, patch_x2, patch_y1, patch_y2 = utils.patch_coordinates_from_center(
+            (coords[0], coords[1]), roi_mask.shape, patch_size)
+
+        # getting coordinates of the patch center. Necessary if the patch is
+        # in border and shifted
+        if center_region_size is not None:
+            p_center_y = patch_y1 + (patch_y2 - patch_y1)//2
+            p_center_x = patch_x1 + (patch_x2 - patch_x1)//2
+            patch_x1, patch_x2, patch_y1, patch_y2 = utils.patch_coordinates_from_center(
+                (p_center_x, p_center_y), roi_mask.shape, center_region_size)
+
+        # Get the overlap with the rois mask and determine the unique labels
+        overlap_on_labels = roi_mask[patch_y1:patch_y2, patch_x1:patch_x2]
+        unique_labels = [label for label in np.unique(overlap_on_labels) if label != 0]
+        detected_labels.update(set(unique_labels))
+        intersection = np.sum(overlap_on_labels > 0)
+
+        # If there's intersection repeat the candidate for each gt that it matched
+        # This will help a faster NMS and correct computation of metrics
+        if intersection > 0:
+            count = 0
+            for label in unique_labels:
+                tp.append(coords)
+                matching_gt.append(label)
+                # Keep which ones are repeted to avoid feature extraction later
+                repeted_tp.append(tp_count)
+                count += 1
+            tp_count += count
+        else:
+            fp.append(coords)
+            repeted_fp.append(fp_count)
+            fp_count += 1
+
+    # Obtain the false negatives
+    detected_labels = list(detected_labels)
+    gt_labels = np.unique(roi_mask)
+    for label in gt_labels:
+        if (label == 0) or (label in detected_labels):
+            continue
+        y, x = np.where(roi_mask == label)
+        y1, y2 = y.min(), y.max()
+        x1, x2 = x.min(), x.max()
+        w, h = x2 - x1, y2 - y1
+        center_x = x1 + w//2
+        center_y = y1 + h//2
+        radius_aprox = np.maximum(w, h) / 2
+        fn.append((center_x, center_y, radius_aprox))
+
+    # Generate the resulting dataframe
+    tp = pd.DataFrame(tp, columns=['x', 'y', 'radius'])
+    tp['label'] = 'TP'
+    tp['repeted_idxs'] = repeted_tp
+    tp['matching_gt'] = matching_gt
+
+    fp = pd.DataFrame(fp, columns=['x', 'y', 'radius'])
+    fp['label'] = 'FP'
+    fp['repeted_idxs'] = np.asarray(repeted_fp) + len(tp)
+    fp['matching_gt'] = None
+
+    fn = pd.DataFrame(fn, columns=['x', 'y', 'radius'])
+    fn['label'] = 'FN'
+    fn['matching_gt'] = None
+    fn['repeted_idxs'] = False
+
+    return tp, fp, fn
+
+
+def get_froc_df_of_many_imgs_features(
+    candidates_df: pd.DataFrame, fns_df: pd.DataFrame, predictions: np.ndarray
+):
+    """Get the standard froc dataframe out of the candidates dataframe of many images
+    and the dataframe of false negatives for those images.
+    Args:
+        candidates (pd.DataFrame): Dataframe of candidates (tp + fp).
+            Check 'get_tp_fp_fn_center_patch_criteria' for more deails
+        fn (pd.DataFrame): Dataframe of fn. Check 'get_tp_fp_fn_center_patch_criteria'
+            for more deails
+        predictions (np.ndarray): prediction scores for the candidates
+    Returns:
+        pd.DataFrame:
+            Rows: tp, fp, fn objects
+            Columns: [
+                x, y, radius, detection_labels, pred_scores, img_id,
+                matching_gt, repeted_idxs, pred_binary, class_labels
+            ]
+    """
+    # Fill metadata for TP and FP
+    df = candidates_df.copy()
+    values = candidates_df.loc[:, 'candidate_coordinates'].values.copy()
+    df.loc[:, ['x', 'y', 'radius']] = np.vstack(values)
+    df.loc[df.label, 'detection_labels'] = 'TP'
+    df.loc[~df.label, 'detection_labels'] = 'FP'
+    df.loc[:, 'pred_scores'] = predictions
+    df.loc[:, 'matching_gt'] = candidates_df.loc[:, 'matching_gt'].values.copy()
+    df.loc[:, 'repeted_idxs'] = candidates_df.loc[:, 'repeted_idxs'].values.copy()
+
+    # Fill metadata for FN
+    if len(fns_df) != 0:
+        fns_df.loc[:, 'detection_labels'] = 'FN'
+        fns_df.loc[:, 'pred_scores'] = 0.
+        fns_df.loc[:, 'matching_gt'] = None
+        fns_df.loc[:, 'repeted_idxs'] = False
+        columns = [
+            'x', 'y', 'radius', 'detection_labels', 'pred_scores',
+            'img_id', 'matching_gt', 'repeted_idxs'
+        ]
+        df = pd.concat([df[columns], fns_df[columns]], ignore_index=True)
+
+    # Add useful columns for froc computation
+    df.loc[:, 'pred_binary'] = False
+    df.loc[:, 'class_labels'] = False
+
+    return df
+
+
+def get_froc_df_of_img(
+    candidates: pd.DataFrame, fn: pd.DataFrame, predictions: np.ndarray, image_id: int
+):
+    """Get the basic dataframe used for froc calculation from the detections
+    of an image.
+    Args:
+        candidates (pd.DataFrame): Dataframe of candidates (tp + fp).
+            Check 'get_tp_fp_fn_center_patch_criteria' for more deails
+        fn (pd.DataFrame): Dataframe of fn. Check 'get_tp_fp_fn_center_patch_criteria'
+            for more deails
+        predictions (np.ndarray): Array of predictions (of the non-duplicated dataset)
+        image_id (int): id of the image considered
+
+    Returns:
+        pd.DataFrame:
+            Rows: tp, fp, fn objects
+            Columns: [
+                x, y, radius, detection_labels, pred_scores, img_id,
+                matching_gt, repeted_idxs, pred_binary, class_labels
+            ]
+    """
+    # Restore repeted detections
+    df = candidates.drop_duplicates(subset='repeted_idxs')
+    df.loc[:, 'pred_scores'] = predictions
+    df = df.loc[candidates.repeted_idxs.tolist(), :]
+
+    # Fill metadata
+    df['img_id'] = image_id
+    df['repeted_idxs'] = candidates.repeted_idxs.tolist()
+    df['matching_gt'] = candidates.matching_gt.tolist()
+    df['detection_labels'] = candidates.label.tolist()
+
+    # Fill metadata for FN
+    if len(fn) != 0:
+        fn['detection_labels'] = 'FN'
+        fn['img_id'] = image_id
+        fn['pred_scores'] = 0.
+        fn['matching_gt'] = None
+        fn['repeted_idxs'] = False
+        df = pd.concat([df, fn], axis=0, ignore_index=True)
+
+    # Add useful columns for froc computation
+    df['pred_binary'] = False
+    df['class_labels'] = False
+
+    return df
+
+
+def non_maximum_suppression_w_labels(froc_df: pd.DataFrame):
+    """ For each grund truth label matched by a detection, keep the detection with
+    the highest prediction score
+    Args:
+        froc_df (pd.DataFrame): Dataframe with prediction results,
+            check function 'get_froc_df_of_img' for more details.
+    Returns:
+        (pd.DataFrame): filtered version of the Dataframe with prediction results.
+    """
+    filtered_froc_df = []
+    for img_id in froc_df.img_id.unique():
+        # get the data for one image
+        img_df = froc_df.loc[froc_df.img_id == img_id].copy()
+        img_df.reset_index(inplace=True, drop=True)
+        # Get the duplicated gts and keep the one with largest pred score
+        img_df.loc[:, 'to_drop'] = False
+        for gt_idx in img_df.matching_gt.dropna().unique():
+            condition = (img_df.matching_gt == gt_idx)
+            if (gt_idx is None) or len(img_df.loc[condition]) == 1:
+                continue
+            else:
+                max_pred = img_df.loc[condition, 'pred_scores'].max()
+                img_df.loc[(condition) & (img_df.pred_scores != max_pred), 'to_drop'] = True
+        # Drop unnecessary stuf
+        img_df = img_df.loc[~img_df.to_drop, img_df.columns != 'to_drop']
+
+        # Add the image data to the filtered dataframe
+        filtered_froc_df.append(img_df)
+    filtered_froc_df = pd.concat(filtered_froc_df, axis=0, ignore_index=True)
+
+    return filtered_froc_df
